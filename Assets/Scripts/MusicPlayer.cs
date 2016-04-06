4bd6915813ef21aaefdc46a78cf4afa74351bd1c
jeANS <jeandsmith27@gmail.com>
2016-04-06 09:17:50 -0700

    Initial Stage of the Project

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private static MusicPlayer instance = null;  //Is not referring to any object
+    void Start()
+    {
+        if (instance != null) //There is an instance of the object
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
