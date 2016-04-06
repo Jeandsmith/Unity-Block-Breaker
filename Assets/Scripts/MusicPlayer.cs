154c1abff83fc569b607dd95723b364649f396cd
jeANS <jeandsmith27@gmail.com>
2016-04-06 11:12:37 -0700

    Addition to Code: Awake() Function and other fixes

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -6,16 +6,32 @@ public class MusicPlayer : MonoBehaviour
 {
 
     private static MusicPlayer instance = null;  //Meaning Is not pointing to anything to any object
+
+    //Run before Start and just after an object is instantiated.
+    private void Awake()
+    {
+       Debug.Log("Music player is Awake" + GetInstanceID()); 
+    }
+
+    //Run this at the start of the scene.
     void Start()
     {
         if (instance != null) //There is an instance of the object
         {
+            Debug.Log("Music player Start" + GetInstanceID());
             Destroy(gameObject);
+            print("Music Player Is Playing.");
         }
         else
         {
             instance = this;
             GameObject.DontDestroyOnLoad(gameObject);
         }
+    }
+
+    // Run this every frame of the game
+    private void Update()
+    {
+        
     }
 }
