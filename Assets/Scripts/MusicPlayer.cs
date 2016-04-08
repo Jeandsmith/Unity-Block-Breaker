9ecb36c062143a723212f8c0b9f8442db7a6a1f3
jeANS <jeandsmith27@gmail.com>
2016-04-08 01:16:01 -0700

    Added Collisions to the Objects and Some Other Gae assets

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,37 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Xml.Serialization;
+
 
 public class MusicPlayer : MonoBehaviour
 {
 
-    private static MusicPlayer instance = null;  //Meaning Is not pointing to anything to any object
+    private static MusicPlayer _instance = null;  //Meaning Is not pointing to anything to any object
 
     //Run before Start and just after an object is instantiated.
     private void Awake()
     {
-       Debug.Log("Music player is Awake: " + GetInstanceID()); 
+        Debug.Log("Music player is Awake: " + GetInstanceID());
+        if (_instance != null) //There is an instance of the object
+        {
+            Debug.Log("Music player Start: " + GetInstanceID()); //The id of the instance does not print to the console
+            Destroy(gameObject);
+            print("Music Player Is Playing.");
+        } else
+        {
+            _instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
     }
 
     //Run this at the start of the scene.
     void Start()
     {
-        if (instance != null) //There is an instance of the object
-        {
-            Debug.Log("Music player Start: " + GetInstanceID());
-            Destroy(gameObject);
-            print("Music Player Is Playing.");
-        }
-        else
-        {
-            instance = this;
-            GameObject.DontDestroyOnLoad(gameObject);
-        }
+
     }
 
     // Run this every frame of the game
     private void Update()
     {
-        
+
     }
 }
